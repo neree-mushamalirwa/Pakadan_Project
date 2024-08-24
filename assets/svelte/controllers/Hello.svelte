9ea9c1dc0e3f490fd476ab016c7299301bfb970a
nereemushamalirwa <nereemushamalirwabm@gmail.com>
2024-08-24 02:50:10 +0200

    first commit continue and manage svelte component

--- assets/svelte/controllers/Hello.svelte
+++ assets/svelte/controllers/Hello.svelte
@@ -1,12 +1,42 @@
 
 <script>
-     let currentTitle = "";
+     let currentTitle
+     let arrayLinks = []
      let special_list
-     let goLeft = true
-     function setTitle() {
+     let goLeft = false
+
+     function setSpecialList1() {
         goLeft = !goLeft
-        console.log("clicked")
-	}
+        currentTitle = "Titre 1"
+        arrayLinks = []
+        arrayLinks = [
+            {key: "/1" , value: "Numérique 1 Titre 1"},
+            {key: "/2" , value: "Numérique 2 Titre 1"},
+            {key: "/3" , value: "Numérique 3 Titre 1"}
+        ]
+	  }
+     function setSpecialList2() {
+        goLeft = !goLeft
+        currentTitle = "Titre 2"
+        arrayLinks = []
+        arrayLinks = [
+            {key: "/4" , value: "Numérique 1 Titre 2"},
+            {key: "/5" , value: "Numérique 2 Titre 2"},
+            {key: "/6" , value: "Numérique 3 Titre 2"}
+        ]
+	  }
+     function setSpecialList3() {
+        goLeft = !goLeft
+        currentTitle = "Titre 3"
+        arrayLinks = []
+        arrayLinks = [
+            {key: "/7" , value: "Numérique 1 Titre 3"},
+            {key: "/8" , value: "Numérique 2 Titre 3"},
+            {key: "/9" , value: "Numérique 3 Titre 3"}
+        ]
+	  }
+
+
     function updateGoLeft(params) {
         goLeft = params
     }
@@ -47,8 +77,8 @@
             <div class="container_items">
                 <ul>
                     <li class="special_list" bind:this={special_list}>
-                        <button on:click={setTitle}>
-                            <span>Visibilité numérique</span>
+                        <button on:click={setSpecialList1}>
+                            <span>Visibilité numérique 1</span>
                             <svg width="20px" height="20px" xmlns="http://www.w3.org/2000/svg" fill="none" viewbox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-6">
                                 <path stroke-linecap="round" stroke-linejoin="round" d="m8.25 4.5 7.5 7.5-7.5 7.5"/>
                             </svg>
@@ -56,8 +86,8 @@
 
                     </li>
                     <li class="special_list">
-                        <button>
-                            <span>Visibilité numérique</span>
+                        <button on:click={setSpecialList2}>
+                            <span>Visibilité numérique 2</span>
                             <svg width="20px" height="20px" xmlns="http://www.w3.org/2000/svg" fill="none" viewbox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-6">
                                 <path stroke-linecap="round" stroke-linejoin="round" d="m8.25 4.5 7.5 7.5-7.5 7.5"/>
                             </svg>
@@ -65,8 +95,8 @@
 
                     </li>
                     <li class="special_list">
-                        <button>
-                            <span>Visibilité numérique</span>
+                        <button on:click={setSpecialList3}>
+                            <span>Visibilité numérique 3</span>
                             <svg width="20px" height="20px" xmlns="http://www.w3.org/2000/svg" fill="none" viewbox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-6">
                                 <path stroke-linecap="round" stroke-linejoin="round" d="m8.25 4.5 7.5 7.5-7.5 7.5"/>
                             </svg>
@@ -78,6 +108,8 @@
     </div>
     <Side goSide={goLeft}
         on:update={event => updateGoLeft(event.detail)}
+        currentTitle={currentTitle}
+        arrayLinks={arrayLinks}
     >
         <span slot="item_title">Title sec</span>
         <ul slot="list_link" >
--- assets/svelte/controllers/Side.svelte
+++ assets/svelte/controllers/Side.svelte
@@ -1,52 +1,58 @@
 <script>
     import { createEventDispatcher } from "svelte";
 
-    export let goSide = false
-    
+    export let goSide = false;
+    export let currentTitle;
+    export let arrayLinks;
 
-    let hideSide = false
-    const dispatch = createEventDispatcher()
+    let hideSide = false;
+    const dispatch = createEventDispatcher();
 
     function hideSideBar() {
-        hideSide = !hideSide
-        goSide = false
-        dispatch("update" , false)
+        hideSide = !hideSide;
+        goSide = false;
+        dispatch("update", false);
     }
 </script>
-    <div class="container_sub_items container_sub_items_secondary" class:goSide={goSide} class:hideSide={hideSide}>
-        <div class="sub_items" >
-            <button class="item_title" on:click={hideSideBar}>
-                <svg
-                    width="25px"
-                    height="25px"
-                    xmlns="http://www.w3.org/2000/svg"
-                    fill="none"
-                    viewbox="0 0 24 24"
-                    stroke-width="1.5"
-                    stroke="currentColor"
-                    class="size-6"
-                >
-                    <path
-                        stroke-linecap="round"
-                        stroke-linejoin="round"
-                        d="M10.5 19.5 3 12m0 0 7.5-7.5M3 12h18"
-                    />
-                </svg>
-                <span>Menu principal</span>
-            </button>
-            <div class="item_title">
-                <slot name="item_title">
-                    <span>Title 1</span>
-                </slot>
-            </div>
-            <div class="container_items">
-                <slot name="list_link">
-                    <ul>
-                        <a href="/">
-                            <li>Link 1</li>
+
+<div
+    class="container_sub_items container_sub_items_secondary"
+    class:goSide
+    class:hideSide
+>
+    <div class="sub_items">
+        <button class="item_title" on:click={hideSideBar}>
+            <svg
+                width="25px"
+                height="25px"
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                viewbox="0 0 24 24"
+                stroke-width="1.5"
+                stroke="currentColor"
+                class="size-6"
+            >
+                <path
+                    stroke-linecap="round"
+                    stroke-linejoin="round"
+                    d="M10.5 19.5 3 12m0 0 7.5-7.5M3 12h18"
+                />
+            </svg>
+            <span>Menu principal</span>
+        </button>
+        <div class="item_title">
+            <span>{currentTitle}</span>
+        </div>
+        <div class="container_items">
+            <ul>
+                {#if arrayLinks}
+                    {#each arrayLinks as item (item.key)}
+                        <a href={item.key}>
+                            <li>{item.value}</li>
                         </a>
-                    </ul>
-                </slot>
-            </div>
+                    {/each}
+                {/if}
+            </ul>
         </div>
     </div>
+</div>
